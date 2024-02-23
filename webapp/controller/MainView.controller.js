533ce7c9ffab683688c548bc404620e5ad16f4c6
Juan-Martin <juanmartin.ibarbalz@gmail.com>
2024-02-23 22:07:29 +0000

    4.5

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -8,12 +8,23 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("logaligroup.invoices.controller.MainView", {
-           
+
             onInit: function () {
-             const oJSONModel = new sap.ui.model.json.JSONModel();
-             const oView = this.getView();
-             oJSONModel.loadData("./model/SelectionScreenMenu.json");
-             oView.setModel(oJSONModel, "selectionScreen");
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("./model/SelectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen");
+            },
+
+            onFilter: function (oEvent) {
+
+            },
+
+            onClearFilter: function () {
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/ShipName", "");
+                oModelSelScreen.setProperty("/CountryKey", "");
             }
+
         });
     });
